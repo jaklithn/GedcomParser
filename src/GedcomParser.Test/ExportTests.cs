ac284f09a870872f8a2d69bde2adcdb0d2d7238a
Jakob Lithner <jakob.lithner@outlook.com>
2018-01-30 19:59:05 +0100

    Modified export to output to current bin/Debug directory

--- src/GedcomParser.Test/ExportTests.cs
+++ src/GedcomParser.Test/ExportTests.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.IO;
+﻿using System.IO;
 using GedcomParser.Entities;
 using GedcomParser.Services;
 using GedcomParser.Test.Services;
@@ -15,17 +14,19 @@ namespace GedcomParser.Test
         public void CanExport()
         {
             // Arrange
+            //const string gedcomFileName = "Kennedy.ged";
+            //const string gedcomFileName = "Windsor.ged";
+            const string gedcomFileName = "StefanFamily.ged";
+            const string zipFileName = "PersonContainer.zip";
+            const string jsonFileName = "Persons.json";
             var currentDir = Directory.GetCurrentDirectory();
             var baseDir = currentDir.Substring(0, currentDir.IndexOf(@"\bin\Debug"));
-            var gedFilePath = Path.Combine(baseDir, "Resources", "StefanFamily.ged");
+            var gedFilePath = Path.Combine(baseDir, "Resources", gedcomFileName);
             var fileParser = new FileParser();
             fileParser.Parse(gedFilePath);
 
             // Act
-            const string jsonFileName = "Persons.json";
-            var directory = new DirectoryInfo(Environment.CurrentDirectory).Parent?.Parent?.Parent?.Parent?.Parent?.FullName ?? "";
-            var subPath = @"GraphDatabaseDemo\src\Neo.Genealogy\Resources\PersonContainer.zip";
-            var zipFilePath = Path.Combine(directory, subPath);
+            var zipFilePath = Path.Combine(currentDir, zipFileName);
             ZipHandler.SaveToZipFile(fileParser.PersonContainer, zipFilePath, jsonFileName);
 
             // Assert
